345bf09508ac60926538c5d56ac232d68be16804
Tetiana Deineka <tata4ka666@i.ua>
2022-11-11 12:30:15 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', throttle(localData, 500));
+
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+function localData() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function getLocalData() {
+  let localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localData !== null) {
+    email.value = localData.email;
+    message.value = localData.message;
+  }
+}
+getLocalData();
+
+feedbackForm.addEventListener('submit', submitData);
+
+function submitData(e) {
+  e.preventDefault();
+  this.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
